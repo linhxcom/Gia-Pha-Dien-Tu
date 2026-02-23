9b7c31c30942db13cc4255261c58ecd179eb5fea
linhxcom <linh.xcom@gmail.com>
2026-02-23 07:38:07 +0700

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -9,8 +9,8 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: 'Gia phả dòng họ Lê Huy',
-  description: 'Gia phả dòng họ Lê Huy — Quản lý gia phả & kết nối cộng đồng dòng họ',
+  title: 'Gia phả dòng họ Hoàng',
+  description: 'Gia phả dòng họ Hoàng — Quản lý gia phả & kết nối cộng đồng dòng họ',
 };
 
 export default function RootLayout({
